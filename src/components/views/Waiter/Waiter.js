7417fe0c1f1ad956bba63571679ac3806368e6d3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-03 22:45:21 +0200

    Add Waiter view

--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -1,6 +1,4 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
-
 import styles from './Waiter.module.scss';
 
 import {Link, Route, Switch} from 'react-router-dom';
@@ -8,19 +6,91 @@ import {Link, Route, Switch} from 'react-router-dom';
 import NewOrder from '../NewOrder/NewOrder';
 import Order from '../Order/Order';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+const demoContent = [
+  {id: '1', status: 'free', order: null},
+  {id: '2', status: 'thinking', order: null},
+  {id: '3', status: 'ordered', order: 123},
+  {id: '4', status: 'prepared', order: 234},
+  {id: '5', status: 'delivered', order: 345},
+  {id: '6', status: 'paid', order: 456},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'free':
+      return (
+        <>
+          <Button>thinking</Button>
+          <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
+        </>
+      );
+    case 'thinking':
+      return (
+        <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/new`}>new order</Button>
+      );
+    case 'ordered':
+      return (
+        <Button>prepared</Button>
+      );
+    case 'prepared':
+      return (
+        <Button>delivered</Button>
+      );
+    case 'delivered':
+      return (
+        <Button>paid</Button>
+      );
+    case 'paid':
+      return (
+        <Button>free</Button>
+      );
+    default:
+      return null;
+  }
+};
+
 const Waiter = () => (
-  <div className={styles.component}>
-    <h2>
-      Waiter view
-    </h2>
-    <ul>
-      <li>
-        <Link to={`${process.env.PUBLIC_URL}/waiter/order/new`}>Tables events new</Link>
-      </li>
-      <li>
-        <Link exact to={`${process.env.PUBLIC_URL}/waiter/order/:id`}>Tables events</Link>
-      </li>
-    </ul>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Order</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order && (
+                <Button component={Link} to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                  {row.order}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
 
     <Switch>
       <Route path={`${process.env.PUBLIC_URL}/waiter/order/new`}>
@@ -30,7 +100,7 @@ const Waiter = () => (
         <Order />
       </Route>
     </Switch>
-  </div>
+  </Paper>
 );
 
 export default Waiter;
