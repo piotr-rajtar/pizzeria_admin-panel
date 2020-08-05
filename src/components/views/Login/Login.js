c2ab98481ce2844d65e6334682c769947fc98da3
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-05 17:55:32 +0200

    Add Login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,14 +1,38 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
-
 import styles from './Login.module.scss';
 
+import Paper from '@material-ui/core/Paper';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
 const Login = () => (
-  <div className={styles.component}>
-    <h2>
-      Login view
-    </h2>
-  </div>
+  <Paper className={styles.component}>
+    <form noValidate>
+      <TextField
+        id="outlined-basic"
+        label="Login"
+        variant="outlined"
+        className={styles.inputField}
+      />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        autoComplete="current-password"
+        variant="outlined"
+        className={styles.inputField}
+      />
+      <Button
+        variant="outlined"
+        color="primary"
+        size="large"
+        className={styles.button}
+      >
+        Login
+      </Button>
+    </form>
+  </Paper>
 );
 
 export default Login;
