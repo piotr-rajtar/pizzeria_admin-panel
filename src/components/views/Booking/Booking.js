de1b52e0bd9b25230ea6b88383b700aedcba2e37
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-07 17:56:53 +0200

    Add event and booking view

--- src/components/views/Booking/Booking.js
+++ src/components/views/Booking/Booking.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 //import PropTypes from 'prop-types';
-
 import styles from './Booking.module.scss';
+
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const Booking = () => {
   const { id } = useParams();
+
+  const demoBooking = [
+    {
+      start: '15:00',
+      duration: 1,
+      people: 2,
+      id: id,
+    },
+  ];
+
   return (
-    <div className={styles.component}>
-
-      <h2>
-        Booking View
-      </h2>
-
-      <h3>
-        Booking ID: {id}
-      </h3>
-
-    </div>
+    <Paper className={styles.component}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Start hour</TableCell>
+            <TableCell>Duration</TableCell>
+            <TableCell>People</TableCell>
+            <TableCell>Booking id</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoBooking.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>
+                {row.start}
+              </TableCell>
+              <TableCell>
+                {row.duration}
+              </TableCell>
+              <TableCell>
+                {row.people}
+              </TableCell>
+              <TableCell>
+                {row.id}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <ButtonGroup className={styles.button} variant="contained" color="primary" aria-label="outlined primary button group">
+        <Button>Cancel</Button>
+        <Button>Modify</Button>
+      </ButtonGroup>
+    </Paper>
   );
 };
 
--- src/components/views/Event/Event.js
+++ src/components/views/Event/Event.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import {useParams} from 'react-router-dom';
 //import PropTypes from 'prop-types';
-
 import styles from './Event.module.scss';
+
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const Event = () => {
   const { id } = useParams();
+
+  const demoEvent = [
+    {
+      start: '10:00',
+      duration: 3,
+      people: 4,
+      id: id,
+    },
+  ];
+
   return (
-    <div className={styles.component}>
-
-      <h2>
-        Event View
-      </h2>
-
-      <h3>
-        Event ID: {id}
-      </h3>
-
-    </div>
+    <Paper className={styles.component}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Start hour</TableCell>
+            <TableCell>Duration</TableCell>
+            <TableCell>People</TableCell>
+            <TableCell>Event id</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {demoEvent.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>
+                {row.start}
+              </TableCell>
+              <TableCell>
+                {row.duration}
+              </TableCell>
+              <TableCell>
+                {row.people}
+              </TableCell>
+              <TableCell>
+                {row.id}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <ButtonGroup className={styles.button} variant="contained" color="primary" aria-label="outlined primary button group">
+        <Button>Cancel</Button>
+        <Button>Modify</Button>
+      </ButtonGroup>
+    </Paper>
   );
 };
 
