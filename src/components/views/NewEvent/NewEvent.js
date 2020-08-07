2e9081e27f216bda049956ea627225effc3bd5b0
Piotr Rajtar <piotrek.rajtar@gmail.com>
2020-08-07 17:45:31 +0200

    Add new event and booking view

--- src/components/views/NewEvent/NewEvent.js
+++ src/components/views/NewEvent/NewEvent.js
@@ -1,14 +1,71 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
-
 import styles from './NewEvent.module.scss';
 
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
 const NewEvent = () => (
-  <div className={styles.component}>
-    <h2>
-      New Event View
-    </h2>
-  </div>
+  <Paper>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Start Hour</TableCell>
+          <TableCell>Duration</TableCell>
+          <TableCell>People</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        <TableCell>
+          <FormControl>
+            <Select>
+              <MenuItem value={10}>10:00</MenuItem>
+              <MenuItem value={10.5}>10:30</MenuItem>
+              <MenuItem value={11}>11:00</MenuItem>
+              <MenuItem value={11.5}>11:30</MenuItem>
+              <MenuItem value={12}>12:00</MenuItem>
+              <MenuItem value={12.5}>12:30</MenuItem>
+              <MenuItem value={13}>13:00</MenuItem>
+            </Select>
+          </FormControl>
+        </TableCell>
+        <TableCell>
+          <FormControl>
+            <Select>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+            </Select>
+          </FormControl>
+        </TableCell>
+        <TableCell>
+          <FormControl>
+            <Select>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+            </Select>
+          </FormControl>
+        </TableCell>
+      </TableBody>
+    </Table>
+    <ButtonGroup className={styles.button} variant="contained" color="primary" aria-label="outlined primary button group">
+      <Button>Add</Button>
+      <Button>Cancel</Button>
+      <Button>Modify</Button>
+    </ButtonGroup>
+  </Paper>
 );
 
 export default NewEvent;
--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,6 +1,5 @@
 import React from 'react';
 //import PropTypes from 'prop-types';
-
 import styles from './Tables.module.scss';
 
 import {Link, Route, Switch} from 'react-router-dom';
@@ -10,45 +9,147 @@ import NewEvent from '../NewEvent/NewEvent';
 import Booking from '../Booking/Booking';
 import Event from '../Event/Event';
 
-const Tables = () => (
-  <div className={styles.component}>
-    <h2>
-      Tables view
-    </h2>
-    <ul>
-      <li>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/new`}>Tables booking new</Link>
-      </li>
-      <li>
-        <Link to={`${process.env.PUBLIC_URL}/tables/booking/:id`}>Table bookings</Link>
-      </li>
-      <li>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/new`}>Tables events new</Link>
-      </li>
-      <li>
-        <Link to={`${process.env.PUBLIC_URL}/tables/events/:id`}>Tables events</Link>
-      </li>
-    </ul>
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Button from '@material-ui/core/Button';
 
-    <Switch>
-      <Route path={`${process.env.PUBLIC_URL}/tables/booking/new`}>
-        <NewBooking />
-      </Route>
-      <Route path={`${process.env.PUBLIC_URL}/tables/booking/:id`}>
-        <Booking />
-      </Route>
-      <Route path={`${process.env.PUBLIC_URL}/tables/events/new`}>
-        <NewEvent />
-      </Route>
-      <Route path={`${process.env.PUBLIC_URL}/tables/events/:id`}>
-        <Event />
-      </Route>
-    </Switch>
+const reservations = [
+  {hour: '10:00', table1: 'RESERVATION', table2: '', table3: 'EVENT', table4: '', table5: 'EVENT', table6: 'RESERVATION'},
+  {hour: '10:30', table1: 'RESERVATION', table2: '', table3: 'EVENT', table4: '', table5: 'EVENT', table6: 'RESERVATION'},
+  {hour: '11:00', table1: '', table2: 'RESERVATION', table3: '', table4: '', table5: '', table6: ''},
+  {hour: '11:30', table1: '', table2: 'RESERVATION', table3: '', table4: 'EVENT', table5: '', table6: ''},
+  {hour: '12:00', table1: '', table2: 'EVENT', table3: 'RESERVATION', table4: 'EVENT', table5: 'RESERVATION', table6: ''},
+  {hour: '12:30', table1: '', table2: 'EVENT', table3: 'RESERVATION', table4: '', table5: 'RESERVATION', table6: ''},
+  {hour: '13:00', table1: 'EVENT', table2: '', table3: '', table4: 'RESERVATION', table5: '', table6: 'EVENT'},
+  {hour: '13:30', table1: 'EVENT', table2: '', table3: '', table4: 'RESERVATION', table5: '', table6: 'EVENT'},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case '':
+      return (
+        <>
+          <Button
+            component={Link}
+            to={`${process.env.PUBLIC_URL}/tables/booking/new`}
+            variant="outlined"
+            color="primary"
+            size="large"
+            className={styles.button}
+          >
+              New Booking
+          </Button>
+          <Button
+            component={Link}
+            to={`${process.env.PUBLIC_URL}/tables/events/new`}
+            variant="outlined"
+            color="primary"
+            size="large"
+            className={styles.button}
+          >
+            New Event
+          </Button>
+        </>
+      );
+    case 'RESERVATION':
+      return (
+        <Button
+          component={Link}
+          to={`${process.env.PUBLIC_URL}/tables/booking/:id`}
+          variant="outlined"
+          color="secondary"
+          size="large"
+          className={styles.button}
+        >
+          RESERVATION
+        </Button>
+      );
+    case 'EVENT':
+      return (
+        <Button
+          component={Link}
+          to={`${process.env.PUBLIC_URL}/tables/events/:id`}
+          variant="outlined"
+          color="secondary"
+          size="large"
+          className={styles.button}
+        >
+          EVENT
+        </Button>
+      );
+    default:
+      return null;
+  }
+};
+
+const Tables = () => {
+
+  return (
+    <Paper className={styles.component}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Hour</TableCell>
+            <TableCell>Table 1</TableCell>
+            <TableCell>Table 2</TableCell>
+            <TableCell>Table 3</TableCell>
+            <TableCell>Table 4</TableCell>
+            <TableCell>Table 5</TableCell>
+            <TableCell>Table 6</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {reservations.map(row => (
+            <TableRow key={row.hour}>
+              <TableCell component="th" scope="row">
+                {row.hour}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table1)}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table2)}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table3)}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table4)}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table5)}
+              </TableCell>
+              <TableCell>
+                {renderActions(row.table6)}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+
+      <Switch>
+        <Route path={`${process.env.PUBLIC_URL}/tables/booking/new`}>
+          <NewBooking />
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/tables/booking/:id`}>
+          <Booking />
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/tables/events/new`}>
+          <NewEvent />
+        </Route>
+        <Route path={`${process.env.PUBLIC_URL}/tables/events/:id`}>
+          <Event />
+        </Route>
+
+      </Switch>
+    </Paper>
+  );
 
 
-
-
-  </div>
-);
+};
 
 export default Tables;
